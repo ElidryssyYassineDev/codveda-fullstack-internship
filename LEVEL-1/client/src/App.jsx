589c7572671c0bdecb0a3ac86962238806e9938a
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-07-03 20:11:03 +0100

    feat(client): scaffold Vite React project and clean boilerplate

---
+++ LEVEL-1/client/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar/>
+      
+
+      <main className="main-content">
+        <div className="container">
+          <p className="placeholder">
+            Product list coming in Milestone 4.
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
