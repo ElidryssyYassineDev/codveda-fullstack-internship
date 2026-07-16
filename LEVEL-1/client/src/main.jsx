573e35a6d4569f86ebb1f6325c386ebe4a0d2e9a
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-07-17 00:28:21 +0100

    feat(client): wire ProductsProvider and ActivityProvider into the app

--- LEVEL-1/client/src/main.jsx
+++ LEVEL-1/client/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { SocketProvider } from './context/SocketContext.jsx'
+import { ProductsProvider } from './context/ProductsContext.jsx'
+import { ActivityProvider } from './context/ActivityContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,7 +15,11 @@ createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <AuthProvider>
           <SocketProvider>
-            <App />
+            <ProductsProvider>
+              <ActivityProvider>
+                <App />
+              </ActivityProvider>
+            </ProductsProvider>
           </SocketProvider>
         </AuthProvider>
       </ThemeProvider>
